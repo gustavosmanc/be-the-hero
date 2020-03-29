fea788b2fdbae02cb3754a27284433253ec0138b
gustavosmanc <gustavosmanc@gmail.com>
2020-03-29 00:38:42 -0300

    Subiu pra API

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.10.111:3333"
+  baseURL: "https://gsmancuzo-be-the-hero.herokuapp.com/"
 });
 
 export default api;
